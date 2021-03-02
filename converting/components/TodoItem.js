dbf8a47ca3e6a4a3aa9789b5d07ff198d40f785c
mattkrebs1974 <mattkrebsemail@gmail.com>
2021-03-02 15:43:10 -0500

    added motes

--- converting/components/TodoItem.js
+++ converting/components/TodoItem.js
@@ -8,6 +8,54 @@ const TodoItem = (props) => {
     // <View style={props.todo.completed ? styles.style1 : styles.style2} >
 
     <View style={styles.other}>
+
+
+//     import React, { useState } from "react";
+// import { CheckBox, Text, StyleSheet, View , TouchableOpacity} from "react-native";
+
+// const App = () => {
+//   const [isSelected, setSelection] = useState(false);
+
+//   return (
+//     <View style={styles.container}>
+//       <View style={styles.checkboxContainer}>
+//         <CheckBox
+//           value={isSelected}
+//           onValueChange={setSelection}
+//           style={styles.checkbox}
+//         />
+//         <Text style={styles.label}>Do you like React Native?</Text>
+//           <TouchableOpacity
+//         onPress={() => alert('Hello, world!')}
+//         style={{ backgroundColor: 'blue' }}>
+//         <Text style={{ fontSize: 20, color: '#fff' }}>Pick a photo</Text>
+//       </TouchableOpacity>
+//    </View>
+//     </View>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+//   checkboxContainer: {
+//     flexDirection: "row",
+//     marginBottom: 20,
+//   },
+//   checkbox: {
+//     alignSelf: "center",
+//   },
+//   label: {
+//     margin: 8,
+//   },
+// });
+
+// export default App;
+
+
     
 <CheckBox style={styles.one}
           value={props.todo.completed}
